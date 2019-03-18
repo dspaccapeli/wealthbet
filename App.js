9e3b8406628613f3987db543771058252f427e7f
Daniele Spaccapeli <>
2019-03-18 11:57:58 +0100

    Skeleton of the Quiz View

--- App.js
+++ App.js
@@ -1,62 +1,83 @@
-import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading, Asset, Font, Icon } from 'expo';
-import AppNavigator from './navigation/AppNavigator';
+import React, { Component } from 'react';
+import { AppRegistry, SectionList, StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
-  state = {
-    isLoadingComplete: false,
-  };
-
-  render() {
-    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
-      return (
-        <AppLoading
-          startAsync={this._loadResourcesAsync}
-          onError={this._handleLoadingError}
-          onFinish={this._handleFinishLoading}
-        />
-      );
-    } else {
-      return (
-        <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <AppNavigator />
-        </View>
-      );
+export default class Quiz extends Component {
+    render() {
+        return (
+            <View>
+                <QuizHeader />
+                <QuestionContainer />
+                <StepBar />
+            </View>
+        );
     }
-  }
-
-  _loadResourcesAsync = async () => {
-    return Promise.all([
-      Asset.loadAsync([
-        require('./assets/images/robot-dev.png'),
-        require('./assets/images/robot-prod.png'),
-      ]),
-      Font.loadAsync({
-        // This is the font that we are using for our tab bar
-        ...Icon.Ionicons.font,
-        // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
-        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-      }),
-    ]);
-  };
-
-  _handleLoadingError = error => {
-    // In this case, you might want to report the error to your error
-    // reporting service, for example Sentry
-    console.warn(error);
-  };
-
-  _handleFinishLoading = () => {
-    this.setState({ isLoadingComplete: true });
-  };
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
+class QuizHeader extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Quiz</Text>
+                <Text>We want to know more about you</Text>
+            </View>
+        );
+    }
+}
+
+
+class QuestionContainer extends Component {
+    render() {
+        return (
+            <View>
+                <Question />
+                <Choices />
+            </View>
+        );
+    }
+}
+
+class Question extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Question 1:</Text>
+                <Text>Do you like Apple?</Text>
+            </View>
+        );
+    }
+}
+
+class Choices extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Yes</Text>
+                <Text>No</Text>
+                <Text>Maybe</Text>
+            </View>
+        );
+    }
+}
+
+class StepBar extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Step 1</Text>
+            </View>
+        );
+    }
+}
+
+
+
+// Quiz
+    // QuizHeader
+    // QuestionContainer
+        // Question
+        // Choices
+    // StepBar
+
+
+// skip this line if using Create React Native App
+// AppRegistry.registerComponent('AwesomeProject', () => SectionListBasics);
